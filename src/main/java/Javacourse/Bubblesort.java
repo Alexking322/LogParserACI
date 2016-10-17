082c4612868a35d34ba178baa8690d07fecc0502
AlexKing3222 <megalex2009@yandex.ru>
2016-10-17 16:55:07 +0400

    Готовый вариант пузырьковой сортировки с комментариями.

--- src/main/java/Javacourse/Bubblesort.java
+++ src/main/java/Javacourse/Bubblesort.java
@@ -30,7 +30,11 @@ public static void  bubbleSort(int[] arr){
 	
     for(int i = arr.length-1 ; i > 0 ; i--){
         for(int j = 0 ; j < i ; j++){
-            
+        	
+        	/*Сравниваем элементы попарно, 
+            если они имеют неправильный порядок, 
+            то меняем местами*/
+        	
             if( arr[j] > arr[j+1] ){
                 int tmp = arr[j];
                 arr[j] = arr[j+1];
