ddaf31687eb3af4bbbdb576ecc155241f1781504
AlexKing3222 <megalex2009@yandex.ru>
2016-10-17 16:48:38 +0400

    Первый блок пузырькового метода сортировки

--- src/main/java/Javacourse/Bubblesort.java
+++ src/main/java/Javacourse/Bubblesort.java
@@ -3,8 +3,22 @@ package Javacourse;
 public class Bubblesort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+int arr[] = new int[(int) (Math.random() * 100)];
+		
+		for(int i = 0; i < arr.length; i++) {
+		    //элементу массива присваивается случайное число от 0 до 99
+		    arr[i] = (int)(Math.random() * 100);
+		    System.out.print(arr[i] + "  ");
+		}
+		      System.out.print("\n");
+		                bubbleSort(arr);
+		                for(int i = 0; i <  arr.length; i++) {
+		                	System.out.print(arr[i] + "  ");
+		    }
+	
+		                
 	}
+	
 
 }
