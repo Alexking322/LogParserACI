7237c43efeed4952e54f65ed5c0983972681aef2
AlexKing3222 <megalex2009@yandex.ru>
2016-10-17 16:51:39 +0400

    2 блок Пузырькового метода сортировки без комментариев

--- src/main/java/Javacourse/Bubblesort.java
+++ src/main/java/Javacourse/Bubblesort.java
@@ -19,6 +19,23 @@ int arr[] = new int[(int) (Math.random() * 100)];
 	
 		                
 	}
-	
 
+
+
+public static void  bubbleSort(int[] arr){
+    
+	
+    for(int i = arr.length-1 ; i > 0 ; i--){
+        for(int j = 0 ; j < i ; j++){
+            
+            if( arr[j] > arr[j+1] ){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+               
+                }	
+                
+            } 
+}
+}
 }
